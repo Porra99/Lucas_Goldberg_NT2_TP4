e7215c730dc2700f4730a17cfd5237f8e77b2ab1
Lucasgoldberg99 <lucasgoldberg99@gmail.com>
2021-07-01 09:25:36 -0300

    second commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+
+  <section class="src-componentes-navbar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      
+      <router-link to="/">
+        <a class="navbar-brand" href="#">Inicio</a>
+      </router-link>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/formulario">
+              <a class="nav-link" href="#">Formulario</a>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/http">
+              <a class="nav-link" href="#">Solicitud Http</a>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'Navbar',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+
+<style scoped lang="css">
+  .src-components-navbar {
+
+  }
+</style>
